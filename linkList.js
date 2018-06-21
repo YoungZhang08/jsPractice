09fdefda14ea046bea2ce719e6d606846744974d
zhangyangyang <895114856@qq.com>
2018-06-21 11:11:52 +0800

    添加链表删除、返回指定元素索引、链表判空、返回链表长度等方法

--- linkList.js
+++ linkList.js
@@ -17,15 +17,15 @@ function linkList() {
 
     // 尾添加
     this.append = function(val) { // 向链表尾部添加一个新的项
-        let node = new Node(val);
-        let current; // current相当于指针，之后从头结点开始移动
+        let node = new Node(val); // 实例化一个新的节点
+        let current;
         
         if(head === null) { // 链表中的第一个节点
-            head = node;
-        } else {
-            current = head;
-            // 循环链表直到找到最后一项，循环结束current指向链表最后一项元素
+            head = node; // 当链表中没有一个节点的时候，将实例化的新节点(地址)赋值给head成为链表的第一个元素，此时node和head的变量值里都保存着内存中的同一个地址
+        } else { // 当链表中有其他元素的时候
+            current = head; // current也指向head指向的内存地址
             while(current.next) {
+                // 循环链表直到找到最后一项，循环结束current指向链表最后一项元素
                 current = current.next;
             }
             // 找到最后一项元素后。将他的next属性指向新元素node，建立连接
@@ -40,20 +40,22 @@ function linkList() {
         // 判断是否越界
         if(pos >= 0 && pos <= length) {
             let node = new Node(val); // 要插入的节点
-            let current = head; // 头节点
+            let current = head; // current指向链表的第一个元素
             let previous; // 要插入节点的前驱节点
             let index = 0;
             
             if(pos === 0) {
-                // pos===0时，直接将头结点
+                // 当要插入到位置为零号位置的时候，将第一个元素在内存中的地址赋值给新实例化的节点的next域，将新节点和原首元素建立连接
                 node.next = current;
-                head = node;
+                head = node; // 更换头指针指向新加元素的地址作为链表的第一个元素
             } else {
-                // 循环链表，找到正确位置，循环完毕，previous和current分别是被添加节点的前驱节点和后继节点
+                // 最开始的时候后继节点指向链表的第一个元素，第一次循环的时候让前驱节点也指向第一个元素，后继节点后移
                 while(index++ < pos) {
+                    // 循环链表，移动前驱节点的指针和后继节点的指针找到新加节点的前方和后方
                     previous = current;
                     current = current.next;
                 }
+                // 找到正确位置之后先连接后继节点再断开前驱和后继连接前驱和新加节点
                 node.next = current;
                 previous.next = node;
             }
@@ -65,6 +67,70 @@ function linkList() {
         }
     };
 
+    // 删除
+    this.removeAt = function(pos) {
+        // 检查是否越界
+        if(pos > -1 && pos < length) {
+            let current = head;
+            let previous;
+            let index = 0;
+
+            if(pos === 0) {
+                // 移除一个元素
+                head = current.next;
+            } else {
+                while(index++ < pos) {
+                    previous = current;
+                    current = current.next;
+                }   
+                // 找到删除的元素位置之后将其的前驱节点和其后继节点相连接
+                previous.next = current.next;
+            }
+            // 更新链表长度
+            length--;
+            return current.val;
+        } else {
+            return null;
+        }
+    };
+
+    // 返回查找元素的索引
+    this.indexOf = function(val) {
+        let current = head;
+        let index = 0;
+
+        while(current) {
+            if(val === current.val) {
+                return index;
+            }
+            index++;
+            current = current.next;
+        }
+        return -1;
+    };
+
+    // 判空
+    this.isEmpty = function() {
+        return length === 0;
+    };
+
+    // 链表长度
+    this.size = function() {
+        return length;
+    };
+
+    // 清空链表
+    this.clear = function() {
+        let current = head;
+        let index = 0;
+        while(current) {
+            this.removeAt(index++);
+            current = current.next;
+        }
+        length = 0;
+        head = null;
+    }
+    // 打印
     this.print = function() {
         let current = head;
 
@@ -75,10 +141,17 @@ function linkList() {
     };
 }
 
+// 测试函数
 (function() {
     let linked = new linkList();
     linked.append(2);
     linked.append(3);
     linked.append(4);
+    linked.insert(2, 6);
+    console.log(linked.removeAt(1)); // 3
+    console.log(linked.indexOf(6)); // 1
+    console.log(linked.size()); // 3
+    linked.clear();
+    console.log(linked.isEmpty()); // true
     linked.print();
 })();
