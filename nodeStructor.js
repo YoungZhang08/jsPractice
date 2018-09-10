9537d342afac80956cdff44c20f857cc50f1cf0b
zhangyangyang <895114856@qq.com>
2018-09-10 11:21:21 +0800

    重修改遍历器

--- nodeStructor.js
+++ nodeStructor.js
@@ -5,27 +5,25 @@ function Node(value) {
 
 Node.prototype[Symbol.iterator] = function() {
     // 在构造函数的原型链上部署Symbol.iterator方法，
-    // 调用该方法会返回遍历器对象iterator，
+    // 调用该方法会返回一个遍历器对象，
     // 调用该对象的next方法，在返回一个值的同时，
     // 自动将内部指针移到下一个实例。
-    var iterator = {next: next};
-
     var current = this;
 
-    function next() {
-        if(current) {
-            var value = current.value;
-            current = current.next;
-            return {
-                done: false,
-                value: value
+    return {
+        next: function() {
+            if(current) {
+                var value = current.value;
+                current = current.next;
+                return {
+                    done: false,
+                    value: value
+                }
+            } else {
+                return {done: true};
             }
-        } else {
-            return {done: true};
         }
     }
-
-    return iterator;
 }
 
 var one = new Node(1);
